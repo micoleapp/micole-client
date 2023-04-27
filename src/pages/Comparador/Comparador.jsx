1a929d9036130fe82a1a2d26d19b6afd33c907ab
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-04-27 11:23:26 -0300

    Update Comparador.jsx

--- src/pages/Comparador/Comparador.jsx
+++ src/pages/Comparador/Comparador.jsx
@@ -8,7 +8,7 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 export default function Comparador() {
   const dispatch = useDispatch();
-  const [Vermas, setVermas] = useState(false)
+  const [Vermas, setVermas] = useState(false);
   const { arrColegios } = useSelector((state) => state.comparador);
   //   const handler = () => {
   //     dispatch(getDataSchools({ colegio }));
@@ -16,80 +16,59 @@ export default function Comparador() {
   return (
     <>
       <div className="min-h-screen">
-        { arrColegios.length > 0 ? 
-        
-      <>
-      
-      <div
-          style={{
-            width: "100%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "flex-end",
-            paddingRight: "5vh",
-
-            // paddingBottom: "2vh",
-          }}
-        >
-          <Button
-            sx={{
-              fontFamily: "Poppins",
-              fontWeight: "600",
-              color: "0D263B",
-              fontSize: "1.5vh",
-            }}
-            variant="contained"
-          >
-            Descargar comparación
-          </Button>
-        </div>
-        <div
-          style={{
-            width: "100%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            paddingBottom: "10vh",
-          }}
-        >
-          <Typography
-            sx={{
-              fontFamily: "Poppins",
-              fontWeight: "600",
-              color: "0D263B",
-              fontSize: "2.5vh",
-            }}
-          >
-            Comparador de Colegios
-          </Typography>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            height: "100%",
-            gap: "3vh",
-            alignItems: "flex-end",
-            justifyContent:'center'
-            
-          }}
-        >
-          <CardsSch  verMas={Vermas} setVermas={setVermas}/>
-        </div>
-      
-      </>
-
-        
-      :
-      <div className="flex justify-center items-center p-5  semibold text-[3vh]">
-          <p>Aún no has seleccionado ningun colegio!</p>
-      </div>
-    
-      }
-        
-    
-    
-    
+        {arrColegios.length > 0 ? (
+          <>
+            <div className="w-full flex items-center justify-end p-5">
+              <Button
+                sx={{
+                  fontFamily: "Poppins",
+                  fontWeight: "600",
+                  color: "0D263B",
+                  fontSize: "1.5vh",
+                }}
+                variant="contained"
+              >
+                Descargar comparación
+              </Button>
+            </div>
+            <div
+              style={{
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                paddingBottom: "10vh",
+              }}
+            >
+              <Typography
+                sx={{
+                  fontFamily: "Poppins",
+                  fontWeight: "600",
+                  color: "0D263B",
+                  fontSize: "2.5vh",
+                }}
+              >
+                Comparador de Colegios
+              </Typography>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                height: "100%",
+                gap: "3vh",
+                alignItems: "flex-end",
+                justifyContent: "center",
+              }}
+            >
+              <CardsSch verMas={Vermas} setVermas={setVermas} />
+            </div>
+          </>
+        ) : (
+          <div className="flex justify-center items-center p-5  semibold text-[3vh]">
+            <p>Aún no has seleccionado ningun colegio!</p>
+          </div>
+        )}
       </div>
     </>
   );
