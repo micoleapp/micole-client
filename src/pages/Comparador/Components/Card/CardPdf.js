f0d06ccc96adef75a5cced26c7fd6df5a4f571a4
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-04-28 16:56:52 -0300

    pdf css

--- src/pages/Comparador/Components/Card/CardPdf.js
+++ src/pages/Comparador/Components/Card/CardPdf.js
@@ -6,22 +6,21 @@ import Pincon from "./svg/PinIcon";
 import { useSelector } from "react-redux";
 
 import DoDisturbIcon from "@mui/icons-material/DoDisturb";
-import AcredComparador from "../drop/Acred";
-import InfraComparador from "../drop/Infra";
-import CloseIcon from "@mui/icons-material/Close";
-import { deleteColegio } from "../../../../redux/ComparadorActions";
-import { useDispatch } from "react-redux";
+import { Document, Page, Text, View, Image } from "@react-pdf/renderer";
+
 export default function CardsSch() {
   const [rating, setRating] = React.useState(null);
   const { arrColegios } = useSelector((state) => state.comparador);
   console.log(arrColegios);
 
   return (
-    <div
+    <Document>
+      <Page size='A4'>
+         <View
       style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
     >
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <div
+      <View style={{ display: "flex", flexDirection: "column" }}>
+        <View
           style={{
             paddingLeft: "15vh",
             display: "flex",
@@ -33,12 +32,37 @@ export default function CardsSch() {
           {arrColegios?.map((c) => {
             return (
               <>
-                <div className={style.containerCard}>
+                <View
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    padding: "1vh",
+                    maxWidth: "35vh",
+                  }}
+                >
                   {/* HEAD */}
-                  <div className={style.cardHead}>
-                    <img src={c.colegio.logo} alt={c.colegio.nombre_colegio} />
-                    <div className={style.cardHead_info}>
-                      <p
+                  <View
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      paddingBottom: "1vh",
+                      gap: "1vh",
+                      minHeight: "12vh",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      boxShadow: "(0px 10px 50px rgba(13, 38, 59, 0.1))",
+                    }}
+                  >
+                       <Image src={c.colegio.logo} alt={c.colegio.nombre_colegio} />
+                    <View
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        margin: "0",
+                        fontSize: "1.5vh",
+                      }}
+                    >
+                      <Text
                         style={{
                           fontWeight: "700",
                           color: "#0D263B",
@@ -46,235 +70,28 @@ export default function CardsSch() {
                         }}
                       >
                         {c.colegio.nombre_colegio}
-                      </p>
-                      <p>{c.colegio.Distrito?.nombre_distrito}</p>
-                    </div>
-                  </div>
-                  <div></div>
-                </div>
+                      </Text>
+                      <Text>{c.colegio.Distrito?.nombre_distrito}</Text>
+                    </View>
+                  </View>
+                </View>
               </>
             );
           })}
-        </div>
-        {/* dire */}
-        <div className="flex flex-row items-center gap-[4rem]">
-          <div className="min-w-[8vh] max-w-[8vh]">
-            <p className="text-[1.7vh] text-[#0D263B] font-semibold  ">
-              Dirección:
-            </p>
-          </div>
-          <div className={style.bodyCard}>
-            {arrColegios?.map((c) => {
-              return (
-                <>
-                  <div className={style.containerCard}>
-                    <div className={style.div}>
-                      <p className="flex text-[1.5vh] max-w-[20vh]">
-                        {c.colegio.direccion}
-                      </p>
-                    </div>
-                  </div>
-                </>
-              );
-            })}
-          </div>
-        </div>
-        {/* tipo esc */}
-        <div className="flex flex-row items-center gap-[4rem]">
-          <div className="min-w-[8vh] max-w-[8vh]">
-            <p className="text-[1.7vh] text-[#0D263B] font-semibold  ">
-              Tipo de escuela:
-            </p>
-          </div>
-
-          <div className={style.bodyCard}>
-            {arrColegios?.map((c) => {
-              return (
-                <>
-                  <div className={style.containerCard}>
-                    <div className={style.div}>
-                      {c.colegio.Categoria?.map((ca) => {
-                        return (
-                          <>
-                            <div
-                              style={{
-                                display: "flex",
-
-                                fontSize: "1.6vh",
-                                flexDirection: "row",
-                              }}
-                            >
-                              <Pincon />
-                              <p className="text-[1.6vh] ">
-                                {ca.nombre_categoria}
-                              </p>
-                            </div>
-                          </>
-                        );
-                      })}
-                    </div>
-                  </div>
-                </>
-              );
-            })}
-          </div>
-        </div>
-        {/* cant. alum */}
-        <div className="flex flex-row  items-center gap-[4rem]">
-          <div className="min-w-[8vh] max-w-[8vh]">
-            <p className="text-[1.7vh] text-[#0D263B] font-semibold">
-              Cant. Alumnos:
-            </p>
-          </div>
-
-          <div className={style.bodyCard}>
-            {arrColegios?.map((c) => {
-              return (
-                <>
-                  <div className={style.containerCard}>
-                    <div className={style.div}>
-                      <p
-                        style={{
-                          fontSize: "1.6vh",
-                        }}
-                      >
-                        {c.colegio.numero_estudiantes} {"alumnos"}
-                      </p>
-                    </div>
-                  </div>
-                </>
-              );
-            })}
-          </div>
-        </div>
-        {/* area */}
-        <div className="flex flex-row items-center gap-[4rem]">
-          <div className="min-w-[8vh] max-w-[8vh]">
-            <p className="text-[1.7vh] text-[#0D263B] font-semibold  ">Área:</p>
-          </div>
-          <div className={style.bodyCard}>
-            {arrColegios?.map((c) => {
-              return (
-                <>
-                  <div className={style.containerCard}>
-                    <div className={style.div}>
-                      <p
-                        style={{
-                          fontSize: "1.6vh",
-                        }}
-                      >
-                        {c.colegio.area} {"m2"}
-                      </p>
-                    </div>
-                  </div>
-                </>
-              );
-            })}
-          </div>
-        </div>
-        {/*  Métodos Aprendizaje  */}
-        <div className="flex flex-row items-start gap-[3rem]">
-          <div className="min-w-[8vh] max-w-[10vh]">
-            <p className="text-[1.7vh] text-[#0D263B] font-semibold  ">
-              Métodos Aprendizaje
-            </p>
-          </div>
-          <div className={style.bodyCard}>
-            {arrColegios?.map((c) => {
-              return (
-                <>
-                  <div className={style.containerCard}>
-                    <div className={style.div}>
-                      {c.colegio.Metodos.length > 0 ? (
-                        c.colegio.Metodos?.map((m) => {
-                          return (
-                            <>
-                              <div
-                                key={m}
-                                style={{
-                                  display: "flex",
-                                  gap: "1vh",
-                                  fontSize: "1.6vh",
-                                  flexDirection: "row",
-                                }}
-                              >
-                                <Pincon />
-                                <p className="text-[1.6vh] ">{m}</p>
-                              </div>
-                            </>
-                          );
-                        })
-                      ) : (
-                        <div className="flex w-full items-center gap-1  pl-2 justify-start">
-                          <DoDisturbIcon
-                            sx={{ color: "#999999", width: "2vh" }}
-                          />
-                          <p className="text-[1.5vh]">Sin especificar</p>
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                </>
-              );
-            })}
-          </div>
-        </div>
-        {/* neuro */}
-        <div className="flex flex-row items-center gap-[4rem]">
-          <div className="min-w-[8vh] max-w-[8vh]">
-            <p className="text-[1.7vh] text-[#0D263B] font-semibold  ">
-              Neurodiversidad:
-            </p>
-          </div>
-
-          <div className={style.bodyCard}>
-            {arrColegios?.map((c) => {
-              return (
-                <>
-                  <div className={style.containerCard}>
-                    <div className={style.div}>
-                      {c.colegio.Dificultades.length > 0 ? (
-                        c.colegio.Dificultades?.map((d) => {
-                          return (
-                            <>
-                              <div
-                                key={d}
-                                style={{
-                                  display: "flex",
-                                  gap: "1vh",
-                                  fontSize: "1.6vh",
-                                  flexDirection: "row",
-                                }}
-                              >
-                                <Pincon />
-                                <p className="text-[1.6vh] ">{d}</p>
-                              </div>
-                            </>
-                          );
-                        })
-                      ) : (
-                        <div className="flex w-full items-center gap-1  pl-2 justify-start">
-                          <DoDisturbIcon
-                            sx={{ color: "#999999", width: "2vh" }}
-                          />
-                          <p className="text-[1.5vh]">Sin Soporte</p>
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                </>
-              );
-            })}
-          </div>
-        </div>
-
-       
-      </div>
-    </div> 
+        </View>
+      </View>
+    </View>
+      </Page>
+      
+    </Document>
+   
   );
 }
- {/* afili */}
-        {/* <div className="flex flex-row items-start gap-[4rem]">
+{
+  /* afili */
+}
+{
+  /* <div className="flex flex-row items-start gap-[4rem]">
           <div className="min-w-[8vh] max-w-[8vh]">
             <p className="text-[1.7vh] text-[#0D263B]  font-semibold   ">
               Afiliaciones:
@@ -300,9 +117,13 @@ export default function CardsSch() {
               );
             })}
           </div>
-        </div> */}
-        {/* infras */}
-        {/* <div className="flex flex-row items-start gap-[4rem]">
+        </div> */
+}
+{
+  /* infras */
+}
+{
+  /* <div className="flex flex-row items-start gap-[4rem]">
           <div className="min-w-[8vh] max-w-[8vh]">
             <p className="text-[1.7vh] text-[#0D263B] font-semibold ">
               Infraestructura:
@@ -326,4 +147,219 @@ export default function CardsSch() {
               );
             })}
           </div>
-        </div> */}
+        // </div> */
+}
+
+{
+  /* dire */
+}
+// <div className="flex flex-row items-center gap-[4rem]">
+//   <div className="min-w-[8vh] max-w-[8vh]">
+//     <p className="text-[1.7vh] text-[#0D263B] font-semibold  ">
+//       Dirección:
+//     </p>
+//   </div>
+//   <div className={style.bodyCard}>
+//     {arrColegios?.map((c) => {
+//       return (
+//         <>
+//           <div className={style.containerCard}>
+//             <div className={style.div}>
+//               <p className="flex text-[1.5vh] max-w-[20vh]">
+//                 {c.colegio.direccion}
+//               </p>
+//             </div>
+//           </div>
+//         </>
+//       );
+//     })}
+//   </div>
+// </div>
+// {/* tipo esc */}
+// <div className="flex flex-row items-center gap-[4rem]">
+//   <div className="min-w-[8vh] max-w-[8vh]">
+//     <p className="text-[1.7vh] text-[#0D263B] font-semibold  ">
+//       Tipo de escuela:
+//     </p>
+//   </div>
+
+//   <div className={style.bodyCard}>
+//     {arrColegios?.map((c) => {
+//       return (
+//         <>
+//           <div className={style.containerCard}>
+//             <div className={style.div}>
+//               {c.colegio.Categoria?.map((ca) => {
+//                 return (
+//                   <>
+//                     <div
+//                       style={{
+//                         display: "flex",
+
+//                         fontSize: "1.6vh",
+//                         flexDirection: "row",
+//                       }}
+//                     >
+//                       <Pincon />
+//                       <p className="text-[1.6vh] ">
+//                         {ca.nombre_categoria}
+//                       </p>
+//                     </div>
+//                   </>
+//                 );
+//               })}
+//             </div>
+//           </div>
+//         </>
+//       );
+//     })}
+//   </div>
+// </div>
+// {/* cant. alum */}
+// <div className="flex flex-row  items-center gap-[4rem]">
+//   <div className="min-w-[8vh] max-w-[8vh]">
+//     <p className="text-[1.7vh] text-[#0D263B] font-semibold">
+//       Cant. Alumnos:
+//     </p>
+//   </div>
+
+//   <div className={style.bodyCard}>
+//     {arrColegios?.map((c) => {
+//       return (
+//         <>
+//           <div className={style.containerCard}>
+//             <div className={style.div}>
+//               <p
+//                 style={{
+//                   fontSize: "1.6vh",
+//                 }}
+//               >
+//                 {c.colegio.numero_estudiantes} {"alumnos"}
+//               </p>
+//             </div>
+//           </div>
+//         </>
+//       );
+//     })}
+//   </div>
+// </div>
+// {/* area */}
+// <div className="flex flex-row items-center gap-[4rem]">
+//   <div className="min-w-[8vh] max-w-[8vh]">
+//     <p className="text-[1.7vh] text-[#0D263B] font-semibold  ">Área:</p>
+//   </div>
+//   <div className={style.bodyCard}>
+//     {arrColegios?.map((c) => {
+//       return (
+//         <>
+//           <div className={style.containerCard}>
+//             <div className={style.div}>
+//               <p
+//                 style={{
+//                   fontSize: "1.6vh",
+//                 }}
+//               >
+//                 {c.colegio.area} {"m2"}
+//               </p>
+//             </div>
+//           </div>
+//         </>
+//       );
+//     })}
+//   </div>
+// </div>
+// {/*  Métodos Aprendizaje  */}
+// <div className="flex flex-row items-start gap-[3rem]">
+//   <div className="min-w-[8vh] max-w-[10vh]">
+//     <p className="text-[1.7vh] text-[#0D263B] font-semibold  ">
+//       Métodos Aprendizaje
+//     </p>
+//   </div>
+//   <div className={style.bodyCard}>
+//     {arrColegios?.map((c) => {
+//       return (
+//         <>
+//           <div className={style.containerCard}>
+//             <div className={style.div}>
+//               {c.colegio.Metodos.length > 0 ? (
+//                 c.colegio.Metodos?.map((m) => {
+//                   return (
+//                     <>
+//                       <div
+//                         key={m}
+//                         style={{
+//                           display: "flex",
+//                           gap: "1vh",
+//                           fontSize: "1.6vh",
+//                           flexDirection: "row",
+//                         }}
+//                       >
+//                         <Pincon />
+//                         <p className="text-[1.6vh] ">{m}</p>
+//                       </div>
+//                     </>
+//                   );
+//                 })
+//               ) : (
+//                 <div className="flex w-full items-center gap-1  pl-2 justify-start">
+//                   <DoDisturbIcon
+//                     sx={{ color: "#999999", width: "2vh" }}
+//                   />
+//                   <p className="text-[1.5vh]">Sin especificar</p>
+//                 </div>
+//               )}
+//             </div>
+//           </div>
+//         </>
+//       );
+//     })}
+//   </div>
+// </div>
+// {/* neuro */}
+// <div className="flex flex-row items-center gap-[4rem]">
+//   <div className="min-w-[8vh] max-w-[8vh]">
+//     <p className="text-[1.7vh] text-[#0D263B] font-semibold  ">
+//       Neurodiversidad:
+//     </p>
+//   </div>
+
+//   <div className={style.bodyCard}>
+//     {arrColegios?.map((c) => {
+//       return (
+//         <>
+//           <div className={style.containerCard}>
+//             <div className={style.div}>
+//               {c.colegio.Dificultades.length > 0 ? (
+//                 c.colegio.Dificultades?.map((d) => {
+//                   return (
+//                     <>
+//                       <div
+//                         key={d}
+//                         style={{
+//                           display: "flex",
+//                           gap: "1vh",
+//                           fontSize: "1.6vh",
+//                           flexDirection: "row",
+//                         }}
+//                       >
+//                         <Pincon />
+//                         <p className="text-[1.6vh] ">{d}</p>
+//                       </div>
+//                     </>
+//                   );
+//                 })
+//               ) : (
+//                 <div className="flex w-full items-center gap-1  pl-2 justify-start">
+//                   <DoDisturbIcon
+//                     sx={{ color: "#999999", width: "2vh" }}
+//                   />
+//                   <p className="text-[1.5vh]">Sin Soporte</p>
+//                 </div>
+//               )}
+//             </div>
+//           </div>
+//         </>
+//       );
+//     })}
+//   </div>
+// </div>
