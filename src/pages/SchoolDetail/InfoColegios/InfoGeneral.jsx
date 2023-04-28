7ada73c12b66768faebbeab8c97f5a24627f044c
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-04-28 14:12:50 -0300

    Update InfoGeneral.jsx

--- src/pages/SchoolDetail/InfoColegios/InfoGeneral.jsx
+++ src/pages/SchoolDetail/InfoColegios/InfoGeneral.jsx
@@ -1,105 +1,88 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
-import { BsPinAngle } from 'react-icons/bs';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { BsPinAngle } from "react-icons/bs";
 export default function InfoGeneral() {
+  const { oneSchool } = useSelector((state) => state.schools);
 
-    const { oneSchool } = useSelector(
-        (state) => state.schools
-    );
+  // lalalalal
 
-// lalalalal
+  return (
+    <section className=" bg-white left flex flex-col gap-8 w-full">
+      <div className=" bg-white flex flex-col gap-2 rounded-md ">
+        <h2 className="font-semibold text-[#0D263B] text-[2.4vh] text-xl">
+          Descripción
+        </h2>
+        <p className=" text-[#696969] text-[1.8vh] ">{oneSchool.descripcion}</p>
+      </div>
+      <div className="bg-white flex flex-col gap-2 rounded-md ">
+        <h2 className="font-semibold  text-[#0D263B] text-[2.4vh]">
+          Propuesta Valor Educativa
+        </h2>
+        <p className=" text-[#696969] text-[1.8vh] ">
+          {oneSchool.propuesta_valor}
+        </p>
+      </div>
 
-    return (
-        <section className=" bg-white left flex flex-col gap-8 w-full">
-
-            <div className=" bg-white flex flex-col gap-2 rounded-md ">
-                <h2 className="font-semibold text-[#0D263B] text-[2.4vh] text-xl">Descripción</h2>
-                <p className=" text-[#696969] text-[1.8vh] ">{oneSchool.descripcion}</p>
-            </div>
-            <div className="bg-white flex flex-col gap-2 rounded-md ">
-                <h2 className="font-semibold  text-[#0D263B] text-[2.4vh]">
-                Propuesta Valor Educativa
-                </h2>
-                <p className=" text-[#696969] text-[1.8vh] ">
-                    {oneSchool.propuesta_valor}
+      <div className=" bg-white flex flex-col gap-5 rounded-md ">
+        <h2 className="font-semibold  text-[#0D263B] text-[2.4vh]">
+          Detalles del Colegio
+        </h2>
+        <div className="flex text-xs w-full flex-col lg:flex-row gap-3 justify-between">
+          <ul className="grid grid-cols-3 w-full gap-3">
+            <li className="text-text-[#0D263B] gap-2">
+              <span className="font-semibold text-[#0D263B]  ">RUC: </span>
+              {oneSchool.ruc}
+            </li>
+            <li className="text-text-[#0D263B] gap-2">
+              <span className="font-semibold text-[#0D263B]  ">Area: </span>
+              {oneSchool.area}
+            </li>
+            <li className="text-text-[#0D263B] gap-2">
+              <span className="font-semibold text-[#0D263B]  ">
+                Fundación:{" "}
+              </span>
+              {oneSchool.fecha_fundacion}
+            </li>
+            <li className="text-text-[#0D263B] gap-2">
+              <span className="font-semibold text-[#0D263B]  ">Niveles: </span>
+              {oneSchool.Nivels?.map((nivel) => nivel.nombre_nivel).join(", ")}
+            </li>
+            <li className="text-text-[#0D263B] gap-2">
+              <span className="font-semibold text-[#0D263B]  ">Director: </span>
+              {oneSchool.nombre_director}
+            </li>
+          </ul>
+          <ul className="flex flex-col gap-3"></ul>
+        </div>
+        {oneSchool?.Metodos?.length > 0 && (
+          <>
+            <h2 className="font-semibold text-lg">Metodos de aprendizaje</h2>
+            <div className="flex flex-wrap gap-5">
+              {oneSchool?.Metodos?.map((metodo) => (
+                <p className="flex gap-2 items-center text-sm">
+                  {" "}
+                  <BsPinAngle className="text-[#0061dd]" />{" "}
+                  {metodo.nombre_metodo}{" "}
                 </p>
+              ))}
             </div>
-     
-
-            <div className=" bg-white flex flex-col gap-5 rounded-md ">
-                <h2 className="font-semibold  text-[#0D263B] text-[2.4vh]">Detalles del Colegio</h2>
-                <div className="flex text-xs w-full flex-col lg:flex-row gap-3 justify-between">
-                    <ul className="grid grid-cols-3 w-full gap-3">
-                        <li className="text-text-[#0D263B] gap-2">
-                            <span className="font-semibold text-[#0D263B]  ">RUC: </span>
-                            {oneSchool.ruc}
-                        </li>
-                        <li className="text-text-[#0D263B] gap-2">
-                            <span className="font-semibold text-[#0D263B]  ">Area: </span>
-                            {oneSchool.area}
-                        </li>
-                        <li className="text-text-[#0D263B] gap-2">
-                            <span className="font-semibold text-[#0D263B]  ">
-                                Fundación:{" "}
-                            </span>
-                            {oneSchool.fecha_fundacion}
-                        </li>
-                        <li className="text-text-[#0D263B] gap-2">
-                            <span className="font-semibold text-[#0D263B]  ">Niveles: </span>
-                            {oneSchool.Nivels?.map((nivel) => nivel.nombre_nivel).join(
-                                ", "
-                            )}
-                        </li>
-                        <li className="text-text-[#0D263B] gap-2">
-                            <span className="font-semibold text-[#0D263B]  ">
-                                Director:{" "}
-                            </span>
-                            {oneSchool.nombre_director}
-                        </li>
-                    </ul>
-                    <ul className="flex flex-col gap-3">
-
-                    </ul>
-
-                </div>
-                {oneSchool?.Metodos?.length > 0 && (
-                    <>
-                        <h2 className="font-semibold text-lg">
-                            Metodos de aprendizaje
-                        </h2>
-                        <div className="flex flex-wrap gap-5">
-                            {oneSchool?.Metodos?.map((metodo) => (
-                                <p className="flex gap-2 items-center text-sm">
-                                    {" "}
-                                    <BsPinAngle className="text-[#0061dd]" />{" "}
-                                    {metodo.nombre_metodo}{" "}
-                                </p>
-                            ))}
-                        </div>
-                    </>
-                )}
-                {oneSchool?.Dificultades?.length > 0 && (
-                    <>
-                        <h2 className="font-semibold text-lg">
-                            Metodos de aprendizaje
-                        </h2>
-                        <div className="flex flex-wrap gap-5">
-                            {oneSchool?.Dificultades?.map((dif) => (
-                                <p className="flex gap-2 items-center text-sm">
-                                    {" "}
-                                    <BsPinAngle className="text-[#0061dd]" />{" "}
-                                    {dif.nombre_dificultad}{" "}
-                                </p>
-                            ))}
-                        </div>
-                    </>
-                )}
+          </>
+        )}
+        {oneSchool?.Dificultades?.length > 0 && (
+          <>
+            <h2 className="font-semibold text-lg">Metodos de aprendizaje</h2>
+            <div className="flex flex-wrap gap-5">
+              {oneSchool?.Dificultades?.map((dif) => (
+                <p className="flex gap-2 items-center text-sm">
+                  {" "}
+                  <BsPinAngle className="text-[#0061dd]" />{" "}
+                  {dif.nombre_dificultad}{" "}
+                </p>
+              ))}
             </div>
-
-           
-    
-
-
-        </section>
-    )
+          </>
+        )}
+      </div>
+    </section>
+  );
 }
