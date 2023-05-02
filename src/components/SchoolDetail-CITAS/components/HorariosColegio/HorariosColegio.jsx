4c6a72f3af88cdf274cc63d34253e427b647d3db
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-05-02 09:17:46 -0300

    horarios citas y drop horarios

--- src/components/SchoolDetail-CITAS/components/HorariosColegio/HorariosColegio.jsx
+++ src/components/SchoolDetail-CITAS/components/HorariosColegio/HorariosColegio.jsx
@@ -1,57 +1,59 @@
-import React, { useState } from "react"
-import {
-    Box,
-    FormControl,
-    InputLabel,
-    MenuItem,
-  
-    Select,
-  } from "@mui/material";
+import React, { useState } from "react";
+import { Box, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 
-function HorariosColegio ({ diaSelecionado , sendDateHs}) {
-    const [horarioColegio, setHorarioColegio] = useState('')
-    // const [validacion, setValidacion] = useState(false)
-    const handleChangeHora = (event) => {
-      setHorarioColegio(event.target.value)
-    }
-    const handlerInfo = (e, date, time,validacion) => {
-      // setValidacion(true)
+function HorariosColegio({ diaSelecionado, sendDateHs }) {
+  const [horarioColegio, setHorarioColegio] = useState("");
+  // const [validacion, setValidacion] = useState(false)
+  const handleChangeHora = (event) => {
+    setHorarioColegio(event.target.value);
+  };
+  const handlerInfo = (e, date, time, validacion) => {
+    // setValidacion(true)
 
-      let infoDiaHora = {
-        time: time,
-        date: date,
-        select:validacion
-      }
-      sendDateHs(infoDiaHora)
-    }
-    return (
-      <>
-        <FormControl
-          sx={{ m: 1, minWidth: 100 }}
-          size="small"
+    let infoDiaHora = {
+      time: time,
+      date: date,
+      select: validacion,
+    };
+
+    sendDateHs(infoDiaHora);
+  };
+
+  // Se convierte el arr  diaSelecionado a un obj y accedemos a la propiedad time que es un array
+  const arrHorarios = diaSelecionado && Object.assign({}, ...diaSelecionado);
+  // console.log(arrHorarios.time);
+  return (
+    <>
+      <FormControl sx={{ m: 1, minWidth: 100 }} size="small">
+        <InputLabel id="demo-select-small">Horarios</InputLabel>
+
+        <Select
+          sx={{ border: "none", outline: "none", fontSize: "2vh" }}
+          labelId="demo-select-small"
+          id="demo-select-small"
+          value={horarioColegio}
+          label={"Horarios"}
+          onChange={handleChangeHora}
         >
-          <InputLabel id="demo-select-small">Horarios</InputLabel>
-  
-          <Select
-            sx={{ border: "none", outline: "none", fontSize: "2vh" }}
-            labelId="demo-select-small"
-            id="demo-select-small"
-            value={horarioColegio}
-            label={"Horarios"}
-            onChange={handleChangeHora}
-          >
-            {diaSelecionado?.map((ele) => {
+          {arrHorarios &&
+            arrHorarios?.time?.map((ele) => {
+              console.log(diaSelecionado);
               return (
-                <MenuItem key={ele.time.desde} onClick={(e) => handlerInfo(e, ele.date, ele.time.desde,true)} value={ele.time.desde}>
-                  {ele.time.desde}/{ele.time.hasta}
+                <MenuItem
+                  key={ele.desde}
+                  onClick={(e) =>
+                    handlerInfo(e, arrHorarios.date, ele.desde, true)
+                  }
+                  value={ele.desde}
+                >
+                  {ele.desde}/{ele.hasta}
                 </MenuItem>
-              )
+              );
             })}
-          </Select>
-        </FormControl>
-      </>
-    )
-  
-  }
+        </Select>
+      </FormControl>
+    </>
+  );
+}
 
-  export default  HorariosColegio
+export default HorariosColegio;
